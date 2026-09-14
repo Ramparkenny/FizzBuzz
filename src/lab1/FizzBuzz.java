956b8efcc86bf39f66c2f59f7981e49bd3170a5a
Kenny <ramparkenny.777@gmail.com>
2026-09-14 16:28:24 -0400

    refactor FizzBuzz into helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,13 +7,12 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+        int i = 1;
 
-            // Find out which numbers divide i.
+        while (i <= 100) {
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
 
-            // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
 
                 System.out.println("Fizz Buzz");
@@ -29,8 +28,9 @@ class FizzBuzz {
             } else {
 
                 System.out.println(i);
-
             }
+
+            i++;
         }
     }
 }
